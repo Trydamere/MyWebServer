d660b93b4e9caf4bca7a53a1907b8422bae041cc
root <304080325@qq.com>
2021-10-04 16:03:21 +0800

    server finished

---
+++ WebServer/EventLoopThread.cpp
@@ -0,0 +1,44 @@
+// @Author Huang Yilong
+// @Email yilong.huang@outlook.com
+#include "EventLoopThread.h"
+#include <functional>
+
+EventLoopThread::EventLoopThread()
+    : loop_(NULL),
+    exiting_(false),
+    thread_(bind(&EventLoopThread::threadFunc, this), "EventLoopThread");
+    mutex_(),
+    cond_(mutex_) {}
+
+EventLoopThread::~EventLoopThread() {
+    exiting_ = true;
+    if(loop_ != NULL) {
+        loop_->quit();
+        thread_.join();
+    }
+}
+
+EventLoop* EventLoopThread::startLoop() {
+    assert(!thread_.started());
+    thread_.start();
+    {
+        MutexLockGuard lock(mutex_);
+        //一直等到threadFun再Thread真正运行
+        while(loop_ == NULL) cond_.wait();
+    }
+    return loop_;
+}
+
+void EventLoopThread::threadFunc() {
+    EventLoop loop;
+
+    {
+        MutexLockGuard lock(mutex_);
+        loop_ = &loop;
+        cond_.notify();
+    }
+
+    loop.loop();
+    //assert(exiting_);
+    loop_ = NULL;
+}
